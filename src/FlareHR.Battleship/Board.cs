dd434d4c9366da05eb306abc4491dae0215b33a9
Paul Gartner <paul.gartner@outlook.com>
2019-01-23 21:17:35 +1100

    Commit of battleship flarehr recruitment exercise

---
+++ src/FlareHR.Battleship/Board.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+
+namespace FlareHR.Battleship
+{
+    public class Board
+    {
+        private readonly Position[] _shipPositions;
+        private readonly Position[] _attackPositions;
+
+        private Board(Position[] shipPositions, Position[] attackPositions)
+        {
+            _shipPositions = shipPositions;
+            _attackPositions = attackPositions;
+        }
+
+        public static Board Create()
+        {
+            return new Board(new Position[0], new Position[0]);
+        }
+
+        public (Board board, bool added) AddShip(Ship ship)
+        {
+            if (ship != null && !_shipPositions.Intersect(ship.OccupiedPositions).Any())
+            {
+                return (new Board(_shipPositions.Concat(ship.OccupiedPositions).ToArray(), _attackPositions), true);
+            }
+
+            return (this, false);
+        }
+
+        public (Board board, bool hit) AddAttack(Position attack) 
+        {
+            if (attack != null)
+            {
+                if (_shipPositions.Any(y => Equals(y, attack)))
+                {
+                    return (new Board(_shipPositions, _attackPositions.Concat(new[] {attack}).ToArray()), true);
+                }
+
+                return (new Board(_shipPositions, _attackPositions.Concat(new[] {attack}).ToArray()), false);
+            }
+
+            return (this, false);
+        }
+
+        public bool AreShipsAfloat()
+        {
+            return _shipPositions.Intersect(_attackPositions).Count() != _shipPositions.Length;
+        }
+    }
+}
---
+++ src/FlareHR.Battleship/Ship.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlareHR.Battleship
+{
+    public class Ship
+    {
+        public Position[] OccupiedPositions { get; }
+
+        private Ship(Position[] occupiedPositions)
+        {
+            OccupiedPositions = occupiedPositions;
+        }
+
+        public static Ship Create(Position bowPosition, Orientation orientation, int shipLength)
+        {
+            IEnumerable<Position> CalculateOccupiedPositions()
+            {
+                var currentX = bowPosition.X;
+                var currentY = bowPosition.Y;
+
+                for (int i = 0; i < shipLength; i++)
+                {
+                    yield return Position.Create(currentX, currentY);
+
+                    if (orientation == Orientation.Horizontal)
+                    {
+                        currentX++;
+                    }
+                    else
+                    {
+                        currentY++;
+                    }
+                }
+            }
+
+            try
+            {
+                var occupiedPositions = CalculateOccupiedPositions().ToArray();
+                return new Ship(occupiedPositions);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return null;
+            }
+        }
+    }
+}
